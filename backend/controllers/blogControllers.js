0cb9c5f05adb53cf8cee93fc1c1dfc25ddff2e68
umashankar <umashankarp33@gmail.com>
2025-02-11 10:36:39 +0530

    few  changes

--- backend/controllers/blogControllers.js
+++ backend/controllers/blogControllers.js
@@ -4,19 +4,19 @@ import asyncHandler from "express-async-handler";
 //@desc     Post a Blog
 //@route    /api/blog
 //@access   Private
-export const postBlog=asyncHandler(async(req,res,next)=>{
-    let {title,content,categories}=req.body;
+export const postBlog = asyncHandler(async (req, res, next) => {
+    let { title, content, categories } = req.body;
 
-    if(!title || !content || !categories){
+    if (!title || !content || !categories) {
         res.status(400);
         throw new Error("Title,content and Categories are required!!");
     }
-    let newBlog=await Blog.create({
-            title,
-            content,
-            categories,
-            author:req?.userId,
-            featuredImage:req.file?.path
+    let newBlog = await Blog.create({
+        title,
+        content,
+        categories,
+        author: req?.userId,
+        featuredImage: req.file?.path
     })
     res.status(201).json(newBlog);
 })
@@ -24,19 +24,32 @@ export const postBlog=asyncHandler(async(req,res,next)=>{
 //@desc     Get Blogs
 //@route    /api/blog
 //@access   Public
-export const getBlogs=asyncHandler(async(req,res)=>{
-    let blogs=await Blog.find();
-    res.status(200).json(blogs)
+export const getBlogs = asyncHandler(async (req, res) => {
+    //get all blogs
+    let categories = req.query.categories;
+    let query = {};
+    if (categories) {
+        query.categories = categories;
+    }
+
+    //pagination
+    let page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 3;
+    let skip = (page - 1) * limit;
+
+    let totalBlogs = await Blog.countDocuments(query);
+    let blogs = await Blog.find(query).populate("author", "username email -_id").sort({ createdAt: -1 }).skip(skip).limit(limit);
+    res.status(200).json({ blogs, current: page, pages: Math.ceil(totalBlogs / limit), totalBlogs });
 })
 
 //@desc     Get a Blog
 //@route    /api/blog/:slug
 //@access   Public
-export const getBlog=asyncHandler(async(req,res)=>{
-    let {slug}=req.params;
-    let blog=await Blog.findOne({slug})
+export const getBlog = asyncHandler(async (req, res) => {
+    let { slug } = req.params;
+    let blog = await Blog.findOne({ slug })
     //update a view
-    blog.views+=1;
+    blog.views += 1;
     await blog.save()
     res.status(200).json(blog)
 })
@@ -44,19 +57,19 @@ export const getBlog=asyncHandler(async(req,res)=>{
 //@desc     Update a Blog
 //@route    /api/blog/:slug
 //@access   Private
-export const updateBlog=asyncHandler(async(req,res)=>{
-    let {slug}=req.params;
+export const updateBlog = asyncHandler(async (req, res) => {
+    let { slug } = req.params;
 
-    let blog=await Blog.findOne({slug})
+    let blog = await Blog.findOne({ slug })
 
-    if(req.body.title){
-        blog.title=req.body.title;
+    if (req.body.title) {
+        blog.title = req.body.title;
     }
-    if(req.body.content){
-        blog.content=req.body.content;
+    if (req.body.content) {
+        blog.content = req.body.content;
     }
-    if(req.file){
-        blog.featuredImage=req.file?.path;
+    if (req.file) {
+        blog.featuredImage = req.file?.path;
     }
 
     await blog.save();
@@ -67,28 +80,28 @@ export const updateBlog=asyncHandler(async(req,res)=>{
 //@desc     Delete a Blog
 //@route    /api/blog/:slug
 //@access   Private
-export const deleteBlog=asyncHandler(async(req,res)=>{
-    let {slug}=req.params;
-    await Blog.findOneAndDelete({slug})
+export const deleteBlog = asyncHandler(async (req, res) => {
+    let { slug } = req.params;
+    await Blog.findOneAndDelete({ slug })
     res.sendStatus(204);
 })
 
 //@desc   Toggle a Like
 //@route  /api/blog/:slug/like
 //access  PrivateQ!`
-export const ToggleLikeBlog=asyncHandler(async(req,res)=>{
-    let {slug}=req.params;
+export const ToggleLikeBlog = asyncHandler(async (req, res) => {
+    let { slug } = req.params;
 
-    let blog=await Blog.findOne({slug});
+    let blog = await Blog.findOne({ slug });
 
-    let userIndex=blog.likes.findIndex((doc)=>{
-        return doc.toString()===req.userId.toString()
+    let userIndex = blog.likes.findIndex((doc) => {
+        return doc.toString() === req.userId.toString()
     })
 
-    if(userIndex===-1){
+    if (userIndex === -1) {
         blog.likes.push(req.userId)
-    }else{
-        blog.likes.splice(userIndex,1);
+    } else {
+        blog.likes.splice(userIndex, 1);
     }
     await blog.save();
 
